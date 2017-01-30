e1bb44fc96607c9f9b3161fcaf4d939bfae71660
byapparov <byapparov@gmail.com>
2017-01-30 22:09:22 +0000

    Fixed the test.

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -2,7 +2,5 @@ context("Hello function unit tests")
 
 
 test_that("Hello function logs message", {
-  expect_message(hello(), "ello")
-  expect_message(hello(), "hi")
-
+  expect_message(hello(), "Hello")
 })
